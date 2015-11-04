2b42095cb4a7eb5f9aec3d1e4e5abc8397d3843b
Sumitro Chatterjee <sumitrochatterjee@gmail.com>
2015-11-03 18:04:57 -0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+function testMinimumSpanningRange() {
+  var Range = [3, 13];
+  var Sets = [
+      [1, 4], [30, 40], [20, 91] ,[8, 10], [6, 7], [3, 9], [9, 12], [11, 14]
+  ];
+  assertEquals(MinimumSpanningRange(Range, Sets), [[3,9], [9,12], [11, 14]], 'Results should be the same.');
+}
