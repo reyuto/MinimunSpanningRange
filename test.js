944dd1ce7ef32cc973d23413a0dc89e5ea8c3baa
Sumitro Chatterjee <sumitrochatterjee@gmail.com>
2015-11-03 18:12:53 -0800

    Update test.js
    Fixed the test.

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ function testMinimumSpanningRange() {
   var Sets = [
       [1, 4], [30, 40], [20, 91] ,[8, 10], [6, 7], [3, 9], [9, 12], [11, 14]
   ];
-  assertEquals(MinimumSpanningRange(Range, Sets), [[3,9], [9,12], [11, 14]], 'Results should be the same.');
+  assertEquals(MinimumSpanningRange(Range, Sets), JSON.stringify([[3,9], [9,12], [11, 14]]), 'Results should be the same.');
 }
